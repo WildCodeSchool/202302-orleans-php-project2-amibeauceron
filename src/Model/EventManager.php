21b89c403c60905a44e9dc29e45b2d3bb9a5a6a4
Justine Clément-Savary <clement.savary.justine@gmail.com>
2023-04-28 10:59:40 +0200

    continu add event

--- src/Model/EventManager.php
+++ src/Model/EventManager.php
@@ -7,4 +7,20 @@ use PDO;
 class EventManager extends AbstractManager
 {
     public const TABLE = 'evenement';
+
+    public function insert(array $event): int
+    {
+        $statement = $this->pdo->prepare(
+            "INSERT INTO " . self::TABLE .
+                " (`title`, `date`, `place`, `description`) " .
+                " VALUES (:title , :date, :place , :description)"
+        );
+
+        $statement->bindValue('title', $event['title'], PDO::PARAM_STR);
+        $statement->bindValue('date', $event['date'], PDO::PARAM_INT);
+        $statement->bindValue('place', $event['place'], PDO::PARAM_STR);
+        $statement->bindValue('description', $event['description'], PDO::PARAM_STR);
+        $statement->execute();
+        return (int)$this->pdo->lastInsertId();
+    }
 }
