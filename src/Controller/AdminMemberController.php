c55bd427a3885e79140e4afc7b218cf86043587f
Aimery Pineau <aimery.pin@gmail.com>
2023-04-28 11:08:43 +0200

    SelectAll(?) 'lastname' pour mieux trier

--- src/Controller/AdminMemberController.php
+++ src/Controller/AdminMemberController.php
@@ -9,7 +9,7 @@ class AdminMemberController extends AbstractController
     public function index(): string
     {
         $memberManager = new MemberManager();
-        $members = $memberManager->selectAll();
+        $members = $memberManager->selectAll('lastname');
 
         return $this->twig->render('Admin/Member/index.html.twig', ['members' => $members]);
     }
